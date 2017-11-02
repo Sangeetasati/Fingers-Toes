a0b4971cc4083c88eae871105f4f162c719d147c
Sangeet Asati <sangeet@medshorts.com>
2017-11-02 14:41:38 -0400

    Skelton ready. Next Button Fix & CSS changes

--- sangeet.js
+++ sangeet.js
@@ -1,23 +1,56 @@
-var number = 0;
+$(document).ready(function(){
+	var interval;
+  var $max = $('#int');
+  var $currentValue = $('#display');
+  var finished = false;
 
-$(document).ready(function () {
-   $("#num").click(function(){
-  var number = parseInt($('#display').text(), 10) || 0;
-  var interval = setInterval(function () {
-      number++;
+  function mytimer () {
+  	//parse a string "0" if the current value is blank
+  	var currentValue = parseInt($currentValue.text().trim() || "0", 10);
+    var max = parseInt($max.val(), 10);
 
-      var max = $('#int').val() || number;
-      $('.box1').css('background-color', number % 3 === 0 ? 'red' : '');
-      $('.box2').css('background-color', number % 5 === 0 ? 'red' : '');
-      $('#display').text(number);
+    //perform the logic if the maximum hasn't been reached
+    if (currentValue < max) {
+    	currentValue++;
 
-      if (number >= max) {
-          clearInterval(interval);
-      }
-  }, 1000);
+      $('.box1').css('background-color', currentValue % 3 === 0 ? 'red' : '');
+      $('.box2').css('background-color', currentValue % 5 === 0 ? 'red' : '');
+      $currentValue.text(currentValue);
+    } else {
+        //stop the timer, marking it as having reached the maximum
+        stoptimer(true);
+    }
+  }
 
-  $('#display').text(number);
+  function starttimer () {
+  	//don't start another interval if one already is started, or if the maximum is not given
+  	if (!interval && $max.val().trim().length) {
+    	//clear previous fields
+    	$currentValue.text('');
+    	$('.box1, .box2').css('background-color', '');
+      //start the new timer
+      interval = setInterval(mytimer, 1000);
+    }
+  }
+
+  function stoptimer (timerFinished) {
+  	//set if the timer stopped because it reached the end
+    finished = (timerFinished === true);
+		if (interval) clearInterval(interval);
+    //remove the interval so we can start a new one later
+    interval = null;
+
+    //clear previous fields if terminated prematurely
+    if (!finished) {
+    	$currentValue.text('');
+    	$('.box1, .box2').css('background-color', '');
+    }
+  }
+
+	$('#num').on('click', function(){
+  	//only start the timer if the last timer didn't finish
+  	if (!finished) starttimer();
+  });
+
+    
 });
-
-});
-var MAX = 'number';
